51f6d19a159ea7b9c00da995c723389282acb26a
Manish Gadhvi <mgadhvi@spartaglobal.com>
2023-05-04 11:21:40 +0100

    Extended the mage class to follow the O of SOLID

---
+++ src/main/java/com/sparta/mg/solid/o/WeaponMage.java
@@ -0,0 +1,9 @@
+package com.sparta.mg.solid.o;
+
+public class WeaponMage extends Mage{
+    private String weapon;
+
+    public WeaponMage(String weapon) {
+        this.weapon = weapon;
+    }
+}
